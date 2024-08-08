e306596a6d38efd12e2a28806aa9b4d54aef6715
Vinicius Coelho <92761467+ViniciusCoelh0@users.noreply.github.com>
2024-08-08 16:48:43 -0300

    petlife.js

--- petlife.js
+++ petlife.js
@@ -1,5 +1,8 @@
 let elementduvida = document.querySelectorAll('.duvida')
 
+alert("Este site foi feito para fins acadêmicos, nada nele é real. |  This site was made for academic purposes, nothing on it is real.)
+
+
 elementduvida.forEach(function (duvida){
     duvida.addEventListener('click', function (){
         duvida.classList.toggle('ativa')
