aba25ae4578b8b5296c0235930830259edc8b49a
Vinicius Coelho <92761467+ViniciusCoelh0@users.noreply.github.com>
2024-08-08 16:55:49 -0300

    petlife.js

--- petlife.js
+++ petlife.js
@@ -1,6 +1,6 @@
 let elementduvida = document.querySelectorAll('.duvida')
 
-alert("Este site foi feito para fins acadêmicos, nada nele é real. |  This site was made for academic purposes, nothing on it is real.)
+alert("Este site foi feito para fins acadêmicos, nada nele é real. |  This site was made for academic purposes, nothing on it is real.")
 
 
 elementduvida.forEach(function (duvida){
